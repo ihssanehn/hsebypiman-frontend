14b883e3741379779e27869eda6adecf99be49ed
Thibaut Faurel <t.faurel@piman-group.fr>
2020-03-31 18:29:32 +0200

    perform ar-form-1

--- src/app/views/pages/analyses-risque/ar-form-1/ar-form-1.component.ts
+++ src/app/views/pages/analyses-risque/ar-form-1/ar-form-1.component.ts
@@ -24,6 +24,31 @@ export class ArForm1Component implements OnInit {
 	// @Output() 
 	// recalculate = new EventEmitter<any>();
   
+	horaire_list:[
+		{display:'1h',value:1},
+		{display:'2h',value:2},
+		{display:'3h',value:3},
+		{display:'4h',value:4},
+		{display:'5h',value:5},
+		{display:'6h',value:6},
+		{display:'7h',value:7},
+		{display:'8h',value:8},
+		{display:'9h',value:9},
+		{display:'10h',value:10},
+		{display:'12h',value:12},
+		{display:'13h',value:13},
+		{display:'14h',value:14},
+		{display:'15h',value:15},
+		{display:'16h',value:16},
+		{display:'17h',value:17},
+		{display:'18h',value:18},
+		{display:'19h',value:19},
+		{display:'20h',value:20},
+		{display:'21h',value:21},
+		{display:'22h',value:22},
+		{display:'23h',value:23},
+		{display:'24h',value:24}
+	]
   	constructor(
 		private router: Router,
 		private activatedRoute: ActivatedRoute,
