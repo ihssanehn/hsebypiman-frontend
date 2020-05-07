51c32b93f2a843a6529e052cbc698a5ed4f14e0e
Thibaut Faurel <t.faurel@piman-group.fr>
2020-05-07 19:32:16 +0200

    resolve tooltip on lists and mat-expansion-panel style

--- src/app/views/pages/chantiers/chantiers.module.ts
+++ src/app/views/pages/chantiers/chantiers.module.ts
@@ -24,7 +24,7 @@ import { ChantierEditComponent } from './chantier-edit/chantier-edit.component';
 import { ChantierAddComponent } from './chantier-add/chantier-add.component';
 import { ChantierDetailComponent } from './chantier-detail/chantier-detail.component';
 import { ChantierFiltersComponent } from './chantier-filters/chantier-filters.component';
-import { NgbDropdownModule, NgbTabsetModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbTabsetModule, NgbTooltipModule, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxMaskModule } from 'ngx-mask';
 
 // Material
@@ -147,6 +147,7 @@ const routes: Routes = [
 		NgbDropdownModule,
 		NgbTabsetModule,
 		NgbTooltipModule,
+		NgbPopoverModule,
 		NgxMaskModule,
 	],
 	providers: [
