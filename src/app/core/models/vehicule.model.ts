89ad9383083cc33ebf02497564b11ee7c5f5cfa7
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-06-09 12:20:24 +0200

    Display search vehicule form

--- src/app/core/models/vehicule.model.ts
+++ src/app/core/models/vehicule.model.ts
@@ -1,11 +1,13 @@
 
 export class Vehicule {
-    _id?: number;
+    id?: number;
     libelle?: string;
+    marque?: string;
     type?: string;
 
-    constructor(type: string, libelle: string = '') {
+    constructor(type: string, libelle: string = '', marque: string = '') {
         this.libelle = libelle;
+        this.marque = marque;
         this.type = type;
     }
 }
