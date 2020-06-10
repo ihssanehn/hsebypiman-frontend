077beaab3f671bf2bd2f2e043ed694a83a1217ea
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-06-10 16:01:19 +0200

    Remove edit function

--- src/app/views/pages/visites-securite/vehicules/visites-vehicule-list/visites-vehicule-list.component.ts
+++ src/app/views/pages/visites-securite/vehicules/visites-vehicule-list/visites-vehicule-list.component.ts
@@ -85,9 +85,7 @@ export class VisitesVehiculeListComponent implements OnInit {
 	viewVisite(visiteId) {
 		this.router.navigate(['../detail', visiteId], { relativeTo: this.activatedRoute });
 	}
-	editVisite(visiteId) {
-		this.router.navigate(['../edit', visiteId], { relativeTo: this.activatedRoute });
-	}
+
 	deleteVisite(visiteId) {
 		Swal.fire({
 			title: 'Désolé cette fonctionnalité n\'a pas encore été implémentée',
