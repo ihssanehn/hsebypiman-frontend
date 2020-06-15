ad1951abfb8789dc1014c516cfccd6b88c567fa9
Thibaut Faurel <t.faurel@piman-group.fr>
2020-06-15 12:54:06 +0200

    resolve Action model

--- src/app/core/models/action.model.ts
+++ src/app/core/models/action.model.ts
@@ -1,6 +1,25 @@
-import { Risque, Equipement, Zone, CatRisqueComment, Chantier } from './';
+import { Status } from './';
+import { User } from '../auth';
 
 export class Action {
     id?: number;
-    
+    actionable_id?: number;
+    actionable_type?: string;
+    actionable?:any;
+    type_id?: number;
+    risque?: string;
+    libelle?: string;
+    objectif?: string;
+    pilote_id?: number;
+    pilote?: User;
+    delai?: Date;
+    status_id?: number;
+    status?: Status;
+    realisation?: Date;
+    efficacite?: string;
+    commentaires?: string;
+    creator_id?: number;
+    creator?: User;
+    created_at?: Date;
+    updated_at?: Date;
 }
