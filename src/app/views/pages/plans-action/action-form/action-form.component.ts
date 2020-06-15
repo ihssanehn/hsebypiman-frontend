78b5268ed5e08389d5597f82fc6863c27d4eaa44
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-06-15 14:07:11 +0200

    Add new Action

--- src/app/views/pages/plans-action/action-form/action-form.component.ts
+++ src/app/views/pages/plans-action/action-form/action-form.component.ts
@@ -14,34 +14,26 @@ import { FormStatus } from '@app/core/_base/crud/models/form-status';
 })
 export class ActionFormComponent implements OnInit {
 
-  typesList: Type[];
-  typesLoaded: boolean = false;
-  usersList: User[];
-  usersLoaded: boolean = false;
-  statusList: Status[];
-  statusLoaded: boolean = false;
-  catHabsList: CatHabilitation[];
-  catHabsLoaded: boolean = false;
-  entreprisesList: Entreprise[];
-  entreprisesLoaded: boolean = false;
-  interimairesList: User[];
-  interimairesLoaded: boolean = false;
-  entrepriseTypesList: Type[];
-  entrepriseTypesLoaded: boolean = false;
-
   @Input() actionForm: FormGroup;
   @Input() formStatus: FormStatus;
   @Input() edit: Boolean;
   @Input() formloading: Boolean;
   @Output() onCancel = new EventEmitter();
   @Output() onSubmit = new EventEmitter();
+
+  typesList: Type[];
+  typesLoaded: boolean = false;
+  usersList: User[];
+  usersLoaded: boolean = false;
+  statusList: Status[];
+  statusLoaded: boolean = false;
+
+
   constructor(
     private typeService:TypeService,
-    private fb: FormBuilder,
     private statusService:StatusService,
-    private catHabilitationService:CatHabilitationService,
-    private entrepriseService: EntrepriseService,
     private userService:UserService,
+    private fb: FormBuilder,
     private cdr: ChangeDetectorRef,
   ) { }
 
@@ -49,10 +41,6 @@ export class ActionFormComponent implements OnInit {
     this.getTypes();
     this.getUsers();
     this.getStatus();
-    this.getCatHabs();
-    this.getEntreprises();
-    this.getEntrepriseTypes();
-    this.getInterimaires();
   }
 
   async getTypes(){
@@ -64,6 +52,7 @@ export class ActionFormComponent implements OnInit {
     }
     this.cdr.markForCheck();
   }
+
   async getUsers(){
     this.usersLoaded = false;
     var res = await this.userService.getList().toPromise();
@@ -73,6 +62,7 @@ export class ActionFormComponent implements OnInit {
     }
     this.cdr.markForCheck();
   }
+
   async getStatus(){
     this.statusLoaded = false;
     var res = await this.statusService.getAllFromModel('Action').toPromise();
@@ -82,74 +72,7 @@ export class ActionFormComponent implements OnInit {
     }
     this.cdr.markForCheck();
   }
-  async getCatHabs(){
-    this.catHabsLoaded = false;
-    var res = await this.catHabilitationService.getAll().toPromise();
-    if(res){
-      this.catHabsList = res.result.data;
-      this.catHabsLoaded = true;
-    }
-    this.cdr.markForCheck();
-  }
-  async getEntreprises(){
-    this.entreprisesLoaded = false;
-    var res = await this.entrepriseService.getListGrouped().toPromise();
-    if(res){
-      this.entreprisesList = res.result.data;
-      this.entreprisesLoaded = true;
-    }
-    this.cdr.markForCheck();
-  }
-  async getEntrepriseTypes(){
-    this.entrepriseTypesLoaded = false;
-    var res = await this.typeService.getAll({'model':'Entreprise'}).toPromise();
-    if(res){
-      this.entrepriseTypesList = res.result.data;
-      this.entrepriseTypesLoaded = true;
-    }
-    this.cdr.markForCheck();
-  }
-  async getInterimaires(){
-    this.interimairesLoaded = false;
-    var res = await this.userService.getAll({'categorie_code':'INTERIMAIRE', 'paginate':false}).toPromise();
-    if(res){
-      this.interimairesList = res.result.data;
-      this.interimairesLoaded = true;
-    }
-    this.cdr.markForCheck();
-  }
-
-  onNoHabCheckChange(event){
-    const habs : FormArray = this.actionForm.get('habilitations') as FormArray;;
-    if(this.actionForm.get('no_hab_required').value){
-      habs.clear();
-      habs.setValidators(null);
-      habs.disable();
-    }else{
-      habs.setValidators([Validators.required]);
-      habs.enable();
-    }
-  }
-
-  onHabCheckChange(event) {
-    const formArray: FormArray = this.actionForm.get('habilitations') as FormArray;
-    if(event.checked){
-      formArray.push(new FormControl(event.source.value));
-    }
-    else{
-      let i: number = 0;
-      formArray.controls.forEach((ctrl: FormControl) => {
-        if(ctrl.value == event.source.value) {
-          formArray.removeAt(i);
-          return;
-        }
-        i++;
-      });
-    }
-  }
-  onHabIsChecked(habId){
-    return this.actionForm.get('habilitations').value.includes(habId);
-  }
+  
   isFieldRequired(controlName){
     if(this.actionForm && this.actionForm.controls[controlName]){
       const control = this.actionForm.controls[controlName]
@@ -161,23 +84,12 @@ export class ActionFormComponent implements OnInit {
     }
     return false
   }
-  isEEFieldRequired(name, index){
-    var ee = this.entreprises.controls[index] as FormGroup;
-    return !!ee.controls[name].validator(name) && ee.controls[name].validator(name).hasOwnProperty('required');
-    return true;
-  }
-  /**
-	 * Checking control validation
-	 *
-	 * @param controlName: string => Equals to formControlName
-	 * @param validationType: string => Equals to valitors name
-	 */
+
 	isControlHasError(controlName: string, validationType: string): boolean {
 		const control = this.actionForm.controls[controlName];
 		if (!control) {
 			return false;
 		}
-
 		const result = control.hasError(validationType) && (control.dirty || control.touched);
 		return result;
   }
@@ -187,46 +99,8 @@ export class ActionFormComponent implements OnInit {
       this.onSubmit.emit(bool)
     }
   }
+
   cancelForm(){
     this.onCancel.emit()
-  }
-  addEntExt(){
-    this.entreprises.push(this.newEntreprises);
-  }
-
-  get entreprises(): FormArray{
-    return this.actionForm.get('entreprises') as FormArray;
-  }
-  get newEntreprises(): FormGroup {
-
-    var new_entreprise = this.fb.group({
-      'type_code':[null, [Validators.required]],
-      'entreprise_id':[null, [Validators.required]],
-      'interimaire_id':[null, [Validators]],
-      'chiffre_affaire':[null, [Validators]],
-      'date_demarrage':[null, [Validators]]
-    });
-    new_entreprise.get('type_code').valueChanges.subscribe(code=>{
-      if(code == 'SOUS_TRAITANT'){
-        new_entreprise.get('interimaire_id').setValidators(null);
-        new_entreprise.get('chiffre_affaire').setValidators(Validators.required);
-      }else{
-        new_entreprise.get('interimaire_id').setValidators(Validators.required);
-        new_entreprise.get('chiffre_affaire').setValidators(null);
-      }
-    })
-    return new_entreprise;
-  }
-
-  removeEe(i){
-    this.entreprises.removeAt(i);
-  }
-  entHasCode(i){
-    if(!this.entrepriseTypesLoaded){
-      return false;
-    }
-    var entreprise : FormGroup = this.entreprises.controls[i] as FormGroup;
-    var code = entreprise.controls['type_code'].value;
-    return code;
   }
 }
