15dc033ebb62e98347d7b5120adcf9b0a36d6656
Thibaut Faurel <t.faurel@piman-group.fr>
2020-06-17 17:50:48 +0200

    update actions-dash

--- src/app/views/pages/plans-action/actions-dash/actions-dash.component.ts
+++ src/app/views/pages/plans-action/actions-dash/actions-dash.component.ts
@@ -8,7 +8,6 @@ import Swal from 'sweetalert2';
 import { EChartOption } from 'echarts';
 import * as echarts from 'echarts';
 import { LayoutConfigService, SparklineChartOptions } from '@app/core/_base/layout';
-import { Widget4Data } from '@app/views/partials/content/widgets/widget4/widget4.component';
 
 
 @Component({
@@ -30,11 +29,6 @@ export class ActionsDashComponent implements OnInit, AfterViewInit, OnDestroy {
 	
 	showFilters:Boolean = false;
 	stats : any;	
-
-	chartOptions1: SparklineChartOptions;
-	chartOptions2: SparklineChartOptions;
-	chartOptions3: SparklineChartOptions;
-	chartOptions4: SparklineChartOptions;
 	
 	
 
@@ -153,27 +147,6 @@ export class ActionsDashComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	ngOnInit() {
 		this.getActionsDash();
-		this.chartOptions1 = {
-			data: [10, 14, 18, 11, 9, 12, 14, 17, 18, 14],
-			color: this.layoutConfigService.getConfig('colors.state.brand'),
-			border: 3
-		};
-		this.chartOptions2 = {
-			data: [11, 12, 18, 13, 11, 12, 15, 13, 19, 15],
-			color: this.layoutConfigService.getConfig('colors.state.danger'),
-			border: 3
-		};
-		this.chartOptions3 = {
-			data: [12, 12, 18, 11, 15, 12, 13, 16, 11, 18],
-			color: this.layoutConfigService.getConfig('colors.state.success'),
-			border: 3
-		};
-		this.chartOptions4 = {
-			data: [11, 9, 13, 18, 13, 15, 14, 13, 18, 15],
-			color: this.layoutConfigService.getConfig('colors.state.primary'),
-			border: 3
-		};
-
 	}
 
 	ngAfterViewInit(){
@@ -237,9 +210,9 @@ export class ActionsDashComponent implements OnInit, AfterViewInit, OnDestroy {
 		}
 		switch (sens) {
 			case 'up':
-				if(value < 0.33){return 'text-danger'}else{return 'text-success'};
+				if(value <= 0.33){return 'text-danger'}else{return 'text-success'};
 			case 'down':
-				if(value < 0.33){return 'text-success'}else{return 'text-danger'};
+				if(value <= 0.33){return 'text-success'}else{return 'text-danger'};
 		}
 	}
 }
