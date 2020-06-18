0d0071e7c369ce4bf17f8b3c4720dc92bf318d0c
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-06-18 16:14:36 +0200

    edit Action efficacite

--- src/app/views/pages/plans-action/action-detail/action-detail.component.ts
+++ src/app/views/pages/plans-action/action-detail/action-detail.component.ts
@@ -12,6 +12,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
 import Swal, { SweetAlertIcon } from 'sweetalert2';
 import { User } from '@app/core/auth';
+import { DateFrToEnPipe, DateEnToFrPipe } from '@app/core/_base/layout';
 
 @Component({
   selector: 'tf-action-detail',
@@ -22,9 +23,11 @@ export class ActionDetailComponent implements OnInit, OnDestroy {
   
   	action: Action;
 	actionForm: FormGroup;
+	formloading: boolean = false;
 	// allRoles: Role[];
 	loaded = false;
 	editMode: boolean = false;
+	editEfficacite: boolean = false;
 	editPilote: boolean = false;
 	usersList: User[];
 	usersLoaded: boolean = false;
@@ -48,6 +51,8 @@ export class ActionDetailComponent implements OnInit, OnDestroy {
 		private userService: UserService,
 		private cdr: ChangeDetectorRef,
 		private permissionsService : NgxPermissionsService,
+		private dateFrToEnPipe: DateFrToEnPipe,
+		private dateEnToFrPipe: DateEnToFrPipe,
 		iconRegistry: MatIconRegistry, 
 		sanitizer: DomSanitizer
 	) {
@@ -56,6 +61,7 @@ export class ActionDetailComponent implements OnInit, OnDestroy {
 	}
 
   	ngOnInit() {
+		this.createForm();
 	  	const routeSubscription = this.activatedRoute.params.subscribe(
 		  	async params => {
 			  	const id = params.id;
@@ -68,10 +74,32 @@ export class ActionDetailComponent implements OnInit, OnDestroy {
 		);
 	}
 
+	createForm() {
+		this.actionForm = this.actionFB.group({
+			type_id: [null, Validators.required],
+			libelle: ['', Validators.required],
+			risque: [''],
+			objectif: ['', Validators.required],
+			pilote_id: [''],
+			delai: [''],
+			realisation: [''],
+			efficacite: [''],
+			commentaires: [''],
+			status_id: [null],
+			visite_type: [null],
+			actionable_id: [null],
+			actionable_type: [null],
+			actionable: [null],
+			type: [null],
+		});
+	}
+
   	async getAction(actionId){
 		try {
 			var res = await this.actionService.get(actionId).toPromise();
 			this.action = res.result.data;
+			this.parseActionDate(res.result.data, 'EnToFr');
+			this.actionForm.patchValue(res.result.data);
 			this.cdr.markForCheck();
 		} catch (error) {
 			console.error(error);
@@ -238,5 +266,54 @@ export class ActionDetailComponent implements OnInit, OnDestroy {
 					timer: 1500
 				});
 			});
+	}
+
+	editEffic(){
+		this.editEfficacite = true;
+	}
+
+	async setEfficacite(efficacite: any){
+		if(efficacite){
+			console.log(efficacite);
+			let form = {...this.actionForm.getRawValue()};
+			this.formloading = true;
+			this.parseActionDate(form, 'FrToEn');
+			form.id = this.action.id;
+			form.efficacite = efficacite;
+
+			const res = await this.actionService.update(form)
+			.toPromise()
+			.then((res) => {
+				
+				this.formloading = false;
+				var code = res.message.code as SweetAlertIcon;
+				var message = res.message.content != 'done' ? '<b class="text-'+code+'">'+res.message.content+'</b>' : null; 
+				Swal.fire({
+					icon: code,
+					title: 'Action mis à jour avec succès',
+					showConfirmButton: false,
+					html: message,
+					timer: code == 'success' ? 1500 : 3000
+				}).then(() => {
+					this.editEfficacite = false;
+					this.getAction(this.action.id);
+				})
+				this.cdr.markForCheck();
+			})
+			.catch(e => {
+				Swal.fire({
+					icon: 'error',
+					title: 'Echec! une erreur est survenue',
+					showConfirmButton: false,
+					timer: 1500
+				});
+			});
+			this.cdr.markForCheck();
+		}
+	}
+
+	parseActionDate(item, direction){
+		item.delai = direction == 'FrToEn' ? this.dateFrToEnPipe.transform(item.delai) : this.dateEnToFrPipe.transform(item.delai);
+		item.realisation = direction == 'FrToEn' ? this.dateFrToEnPipe.transform(item.realisation) : this.dateEnToFrPipe.transform(item.realisation);
 	}
 }
