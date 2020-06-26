345f82f5a9d79b2b9ef3bd34eb6ccd6f8dc124ea
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-06-26 14:05:40 +0200

    Add salarie filter component

--- src/app/views/pages/salaries/salarie-filters/salarie-filters.component.ts
+++ src/app/views/pages/salaries/salarie-filters/salarie-filters.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, EventEmitter, Output, ChangeDetectorRef } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DateFrToEnPipe } from '@app/core/_base/layout';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'tf-salarie-filters',
   templateUrl: './salarie-filters.component.html',
   styleUrls: ['./salarie-filters.component.scss']
 })
-export class SalarieFiltersComponent implements OnInit {
+export class SalarieFiltersComponent implements  OnInit, AfterViewInit {
 
-  constructor() { }
+  @Output() change = new EventEmitter();
 
-  ngOnInit() {
+  filterForm: FormGroup;
+  loading = false;
+  hidden = true;
+  data: boolean = false;
+  fonctions: Object[];
+
+  constructor(
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private dateFrToEnPipe:DateFrToEnPipe,
+		iconRegistry: MatIconRegistry, 
+    sanitizer: DomSanitizer,
+  ) {
+    iconRegistry.addSvgIcon(
+      'search',sanitizer.bypassSecurityTrustResourceUrl('./assets/media/hse-svg/search.svg'));
   }
 
+  ngOnInit() {
+    //this.getFonctions();
+    this.initFiltersForm();
+    this.filterForm.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(data => this.search(data));
+  }
+
+  ngAfterViewInit(){
+  }
+
+  initFiltersForm(){
+    this.filterForm = this.fb.group({
+      fonction_id:[null],
+      score_min:[null],
+      score_max:[null],      
+      created_at_start:[null],
+      created_at_end:[null]
+    })
+  }
+
+  search(filters: any): void {
+    var filter = {...this.filterForm.getRawValue()}
+    this.change.emit(filter);
+  }
+
+  formHasValue(key){
+    return this.filterForm.get(key).value ? true:false;
+  }
+  
+  clearValue(key){
+    this.filterForm.get(key).patchValue(null);
+  }
+ 
 }
