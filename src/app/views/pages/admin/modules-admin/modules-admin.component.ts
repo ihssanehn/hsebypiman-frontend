1a96b071e8b95dc7df65ee15dcc7e5db55f909c1
Thibaut Faurel <t.faurel@piman-group.fr>
2020-08-17 14:24:47 +0200

    resolve modules_admin issue

--- src/app/views/pages/admin/modules-admin/modules-admin.component.ts
+++ src/app/views/pages/admin/modules-admin/modules-admin.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ModuleService } from '@app/core/services/module.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import {extractErrorMessagesFromErrorResponse} from '@app/core/_base/crud';;
+import {FormStatus} from '@app/core/_base/crud/models/form-status';
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from "@angular/forms";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'tf-modules-admin',
@@ -17,6 +20,9 @@ export class ModulesAdminComponent implements OnInit {
   ){
   }
 
+  formloading: boolean = false;
+  formStatus = new FormStatus();
+
   modulesList: any;
 
   ngOnInit() {
@@ -31,12 +37,38 @@ export class ModulesAdminComponent implements OnInit {
   }
 
   onSubmit(){
+    this.formloading = true;
     var params = {modules: this.modulesList};
-    this.moduleService.updateModules(params).subscribe(res=>{
+    this.moduleService.updateModules(params).toPromise()
+    .then(res=>{
       this.modulesList = res.result.data;
       this.moduleService.getModules().toPromise();
       this.cdr.markForCheck();
-    })
+      Swal.fire({
+        icon: 'success',
+        title: 'Modules mis à jour avec succès',
+        showConfirmButton: false,
+        timer: 1500
+      }).then(()=>{
+        this.formloading = false;
+      })
+    }).catch(err =>{ 
+      this.formloading = false;
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Echec! le formulaire est incomplet',
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+      if(err.status === 422){
+        var messages = extractErrorMessagesFromErrorResponse(err);
+        this.formStatus.onFormSubmitResponse({success: false, messages: messages});
+        this.cdr.markForCheck();
+      }
+
+    });
   }
   
   onModuleCheckChange(event, modId) {
