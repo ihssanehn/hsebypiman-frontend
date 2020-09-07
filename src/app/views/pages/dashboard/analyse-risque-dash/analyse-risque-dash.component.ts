468180a460cbebf3e4595e33e3232565677d2af4
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2020-09-07 10:10:22 +0200

    feat: add more dash

--- src/app/views/pages/dashboard/analyse-risque-dash/analyse-risque-dash.component.ts
+++ src/app/views/pages/dashboard/analyse-risque-dash/analyse-risque-dash.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { DashboardService } from '@app/core/services';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'tf-analyse-risque-dash',
@@ -8,9 +9,39 @@ import { DashboardService } from '@app/core/services';
 })
 export class AnalyseRisqueDashComponent implements OnInit {
 
+  @ViewChild('evolArs', {static: true}) evolArs: ElementRef;
+
   stats : any;
   filter: any = {
 		keyword: ""
+  };
+  echartsArEvol;
+  EvolArOptions = {
+		tooltip: {
+			trigger: 'axis'
+		},
+		grid: {
+			left: '1%',
+			right: '4%',
+			bottom: '2%',
+			top: '4%',
+			containLabel: true
+		},
+		xAxis: {
+			name: 'Mois',
+			type: 'category',
+			data: []
+		},
+		yAxis: {
+			type: 'value'
+		},
+		series: [
+			{
+				type: 'line',
+				color: '#37a2da',
+				data: []
+			}
+		]
 	};
 
   constructor(
@@ -22,10 +53,22 @@ export class AnalyseRisqueDashComponent implements OnInit {
     this.getArDash();
   }
 
+  ngAfterViewInit(){
+		this.echartsArEvol = echarts.init(this.evolArs.nativeElement)
+		this.echartsArEvol.showLoading();
+	}
+
   async getArDash() {
 		try {
 			this.dashboardService.getArStats(this.filter).subscribe(res=>{
           this.stats = res.result.data;
+
+          // Ars Evolution
+          this.EvolArOptions.series[0]['data'] = this.stats.total_ars_evolution;
+          this.EvolArOptions.xAxis.data = this.stats.total_ars_evolutionAxis;
+          this.echartsArEvol.setOption(this.EvolArOptions);
+          this.echartsArEvol.hideLoading();
+
           this.cdr.markForCheck();
 				}	
 			);
--- src/app/views/pages/dashboard/visite-dash/visite-dash.component.ts
+++ src/app/views/pages/dashboard/visite-dash/visite-dash.component.ts
@@ -11,18 +11,15 @@ export class VisiteDashComponent implements OnInit {
 
   @ViewChild('evolVss', {static: true}) evolVss: ElementRef;
   @ViewChild('evolNc', {static: true}) evolNc: ElementRef;
+  @ViewChild('pieVssType', {static: true}) pieVssType: ElementRef;
 
   stats : any;
   filter: any = {
 		keyword: ""
   };
-  echartsVssEvol;
-  echartsNcEvol;
-  EvolVssOptions = {
-		// title: {
-		// 	text: 'Évolution des visites sécurité',
-		// 	x: 'left'
-		// },
+  	echartsVssEvol;
+  	echartsNcEvol;
+  	EvolVssOptions = {
 		tooltip: {
 			trigger: 'axis'
 		},
@@ -48,10 +45,6 @@ export class VisiteDashComponent implements OnInit {
 		  ]
 	};
 	EvolNcOptions = {
-		// title: {
-		// 	text: 'Évolution des non conformité',
-		// 	x: 'center'
-		// },
 		tooltip: {
 			trigger: 'axis'
 		},
@@ -78,6 +71,36 @@ export class VisiteDashComponent implements OnInit {
 			}
 		]
 	};
+	echartsVssType;
+	byTypeOptions = {
+		grid: {
+		  left: '3%',
+		  right: '4%',
+		  bottom: '10%',
+		  containLabel: true
+		},
+		tooltip: {
+		  trigger: 'item',
+		  formatter: '{b} : {c} ({d}%)'
+		},
+		legend: {
+		  type: 'scroll',
+		  orient: 'horizontal',
+		  left: 20,
+		  right: 20,
+		  bottom: 10,
+		  series:[]
+		},
+		series: [
+		  {
+		  type: 'pie',
+		  radius: '60%',
+		  center: ['50%', '50%'],
+		  selectedMode: 'single',
+		  data: []
+		  }
+		]
+	  };
 
   constructor(
     private dashboardService: DashboardService,
@@ -93,6 +116,8 @@ export class VisiteDashComponent implements OnInit {
 		this.echartsVssEvol.showLoading();
 		this.echartsNcEvol = echarts.init(this.evolNc.nativeElement)
 		this.echartsNcEvol.showLoading();
+		this.echartsVssType = echarts.init(this.pieVssType.nativeElement)
+		this.echartsVssType.showLoading();
 	}
 
   async getVisiteDash() {
@@ -100,27 +125,32 @@ export class VisiteDashComponent implements OnInit {
 			this.dashboardService.getVsStats(this.filter).subscribe(res=>{
           this.stats = res.result.data;
 
-					// Vss Evolution
-					this.EvolVssOptions.series = [];
-					if(this.stats.total_vss_evolution){
-					  this.stats.total_vss_evolution.forEach(element => {
-						this.EvolVssOptions.series.push({
-						  type: 'line',
-						  name: element.name,
-						  data: element.data
-						});
-						this.EvolVssOptions.legend.data.push(element.name);
-					  });
-					}
-					this.EvolVssOptions.xAxis.data = this.stats.evolutionAxis;
-					this.echartsVssEvol.setOption(this.EvolVssOptions);
-					this.echartsVssEvol.hideLoading();
+			// Actions By Status
+			this.byTypeOptions.series[0]['data'] = this.stats.total_vss_by_type;
+			this.echartsVssType.setOption(this.byTypeOptions);
+			this.echartsVssType.hideLoading();
 
-					// Nc Evolution
-					this.EvolNcOptions.series[0]['data'] = this.stats.total_nc_evolution;
-					this.EvolNcOptions.xAxis.data = this.stats.evolutionAxis;
-					this.echartsNcEvol.setOption(this.EvolNcOptions);
-					this.echartsNcEvol.hideLoading();
+			// Vss Evolution
+			this.EvolVssOptions.series = [];
+			if(this.stats.total_vss_evolution){
+				this.stats.total_vss_evolution.forEach(element => {
+				this.EvolVssOptions.series.push({
+					type: 'line',
+					name: element.name,
+					data: element.data
+				});
+				this.EvolVssOptions.legend.data.push(element.name);
+				});
+			}
+			this.EvolVssOptions.xAxis.data = this.stats.evolutionAxis;
+			this.echartsVssEvol.setOption(this.EvolVssOptions);
+			this.echartsVssEvol.hideLoading();
+
+			// Nc Evolution
+			this.EvolNcOptions.series[0]['data'] = this.stats.total_nc_evolution;
+			this.EvolNcOptions.xAxis.data = this.stats.evolutionAxis;
+			this.echartsNcEvol.setOption(this.EvolNcOptions);
+			this.echartsNcEvol.hideLoading();
 
           this.cdr.markForCheck();
 				}	
