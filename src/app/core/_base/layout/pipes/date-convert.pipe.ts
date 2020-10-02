31f32f656a106618ff0b0fb79023bfeaa9489dcf
tibo <t.faurel@piman-group.fr>
2020-10-02 15:00:43 +0200

    resolve moment

--- src/app/core/_base/layout/pipes/date-convert.pipe.ts
+++ src/app/core/_base/layout/pipes/date-convert.pipe.ts
@@ -1,6 +1,6 @@
 // Angular
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
+import moment from 'moment';
 import { MatExpansionPanelDescription } from '@angular/material';
 
 /**
