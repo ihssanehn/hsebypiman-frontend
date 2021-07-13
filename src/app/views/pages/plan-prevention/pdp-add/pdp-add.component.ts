f4755d7641f1ec935e2699e6d65933a2bc32f987
ihssane hanzaz <i.hanzaz@piman-group.fr>
2021-07-13 17:32:40 +0100

    wip edit and delete

--- src/app/views/pages/plan-prevention/pdp-add/pdp-add.component.ts
+++ src/app/views/pages/plan-prevention/pdp-add/pdp-add.component.ts
@@ -38,21 +38,20 @@ export class PdpAddComponent implements OnInit, OnDestroy {
 
 	ngOnInit() {
 		this.createForm();
-		const routeSubscription = this.activatedRoute.queryParams
+		const routeSubscription = this.activatedRoute.params
 			.subscribe(
 				async params => {
-					const id = params.ar_id;
+					const id = params.id;
+					console.log(params);
 					if (id) {
 						this.pdpService
 							.get(id)
 							.pipe(
 								tap(
 									pdp => {
-										// var _ar = ar.result.data;
-										// _ar.date_accueil_secu = this.dateEnToFrPipe.transform(_ar.date_accueil_secu);
-										// _ar.date_validite = this.dateEnToFrPipe.transform(_ar.date_validite);
-										// this.arForm.patchValue(ar.result.data);
-										// this.fillForm(ar.result.data);
+										const _pdp = pdp.result.data;
+										this.pdpForm.patchValue(_pdp);
+										this.formPathValues(_pdp);
 										console.log(pdp);
 									}
 								)
@@ -129,7 +128,6 @@ export class PdpAddComponent implements OnInit, OnDestroy {
 
 	async onSubmit() {
 		try {
-
 			console.log(this.pdpForm.valid, this.pdpForm);
 			this.pdpForm.markAllAsTouched();
 			if (this.pdpForm.valid) {
@@ -152,6 +150,183 @@ export class PdpAddComponent implements OnInit, OnDestroy {
 			throw error;
 		}
 
+	}
+
+	formPathValues(pdp: Pdp) {
+		console.log(pdp);
+		if (pdp.is_presence_site_client) {
+			this.pdpForm.get('presence_site_client_frequency_id').enable();
+		}
+
+		const consignesArray: FormArray = this.pdpForm.get('consignes') as FormArray;
+		consignesArray.patchValue(pdp.pdp_consigne_ee.map(v => {
+			return {id: v.consigne_ee_id, answer: v.answer, type_operation: v.type_operation, comment: v.comment};
+		}));
+		consignesArray.controls.map((c: FormGroup) => {
+			if (c.get('answer').value) {
+				c.get('comment').enable();
+				if (c.get('type_operation')) {
+					c.get('type_operation').enable();
+				}
+				c.updateValueAndValidity();
+			}
+		});
+
+		const epiDispositionArray: FormArray = this.pdpForm.get('epi_disposition') as FormArray;
+		epiDispositionArray.patchValue(pdp.pdp_epi_disposition_ee.map(v => {
+			return {
+				id: v.epi_disposition_ee_id,
+				answer: v.answer,
+				answer_id: v.answer_id,
+				is_ee: v.is_ee,
+				is_eu: v.is_eu,
+				is_sous_traitant: v.is_sous_traitant,
+				comment: v.comment
+			};
+		}));
+		epiDispositionArray.controls.map((c: FormGroup) => {
+			if (c.get('answer').value) {
+				c.get('comment').enable();
+				c.get('is_ee').enable();
+				c.get('is_eu').enable();
+				c.get('is_sous_traitant').enable();
+				if (c.get('answer_id')) {
+					c.get('answer_id').enable();
+				}
+				c.updateValueAndValidity();
+			}
+		});
+
+		const moyenDisposition: FormArray = this.pdpForm.get('moyen_disposition_ees') as FormArray;
+		moyenDisposition.patchValue(pdp.pdp_moyen_disposition_ee.map(v => {
+			return {
+				id: v.moyen_disposition_ee_id,
+				answer: v.answer,
+				comment: v.comment
+			};
+		}));
+		moyenDisposition.controls.map((c: FormGroup) => {
+			if (c.get('answer').value) {
+				c.get('comment').enable();
+				c.updateValueAndValidity();
+			}
+		});
+
+		const travauxDangereux: FormArray = this.pdpForm.get('travaux_dangereux') as FormArray;
+		travauxDangereux.patchValue(pdp.pdp_travaux_dangereux.map(v => {
+			return {
+				id: v.travaux_dangereux_id,
+				answer: v.answer
+			};
+		}));
+
+		const risques: FormArray = this.pdpForm.get('cat_pdp_risques') as FormArray;
+		risques.patchValue(pdp.pdp_answer_risques.map(v => {
+			return {
+				id: v.cat_pdp_risque_id,
+				answer: v.answer,
+				comment: v.comment,
+				answer_id: v.answer_id,
+				is_ee: v.is_ee,
+				is_eu: v.is_eu,
+				is_sous_traitant: v.is_sous_traitant,
+				other_cat_pdp_risque: v.other_cat_pdp_risque,
+				other_pdp_moyen_risque: v.other_pdp_moyen_risque,
+				other_pdp_situation_risque: v.other_pdp_situation_risque,
+				moyen: [...v.moyens.map(m => {
+					return {
+						id: m.pdp_risque_id,
+						answer: m.answer,
+						comment: m.comment,
+						pdp_risque_moyen_filtre: m.moyen_filter.map(mf => {
+							return {
+								id: mf.pdp_risque_moyen_filter_id,
+								answer: mf.answer,
+								comment: mf.comment,
+								answer_id: mf.answer_id
+							};
+						}),
+					};
+				})],
+				situation: v.situation
+			};
+		}));
+		risques.controls.map((c: FormGroup) => {
+			if (c.get('answer').value) {
+				c.get('comment').enable();
+				c.get('is_eu').enable();
+				c.get('is_piman').enable();
+				c.get('is_sous_traitant').enable();
+				c.get('other_cat_pdp_risque').enable();
+				c.get('other_pdp_situation_risque').enable();
+				(c.get('other_pdp_moyen_risque') as FormArray).controls.map((v: FormGroup) => {
+					v.get('comment').enable();
+				});
+				(c.get('moyen') as FormArray).controls.map((v: FormGroup) => {
+					v.get('answer').enable();
+					if (v.get('answer').value && v.get('is_with_comment').value) {
+						v.get('comment').enable();
+					}
+					(v.get('pdp_risque_moyen_filtre') as FormArray).controls.map(f => {
+						f.get('answer').enable();
+						if (f.get('answer').value && f.get('is_with_comment').value) {
+							f.get('comment').enable();
+						}
+					});
+				});
+				(c.get('situation') as FormArray).controls.map((v: FormGroup) => {
+					v.get('answer').enable();
+					if (v.get('answer').value && v.get('is_with_comment').value) {
+						v.get('comment').enable();
+					}
+				});
+			}
+		});
+		// moyenDisposition.controls.map((c: FormGroup) => {
+		// 	if (c.get('answer').value) {
+		// 		c.get('comment').enable();
+		// 		c.updateValueAndValidity();
+		// 	}
+		// });
+
+		this.pdpForm.updateValueAndValidity();
+		// consignesArray..map(e => {
+		// 	pdp.pdp_consigne_ee.forEach(element => {
+		// 		if (e.id === element.consigne_ee_id) {
+		// 			e.se
+		// 			console.log(e, element);
+		// 			e.answer = element.answer;
+		// 			e.comment = element.comment;
+		// 			e.type_operation = element.type_operation;
+		// 			return;
+		// 		}
+		// 	});
+		// })
+		console.log(consignesArray);
+
+		// const equipementFormArray: FormArray = this.arForm.get('equipements') as FormArray;
+		// ar.equipements.forEach(element => {
+		// 	equipementFormArray.push(new FormControl(element.id));
+		// });
+		//
+		// const zoneFormArray: FormArray = this.arForm.get('zones') as FormArray;
+		// ar.zones.forEach(element => {
+		// 	zoneFormArray.push(new FormControl(element.id));
+		// });
+		//
+		// const commentsFormArray: FormArray = this.arForm.get('comments') as FormArray;
+		// ar.cat_risques.forEach(element => {
+		// 	const commentGroup: FormGroup = this.arFB.group({
+		// 		'cat_risque_id': element.id,
+		// 		'commentaire': element.commentaire
+		// 	});
+		// 	commentsFormArray.push(commentGroup);
+		// });
+		//
+		// this.arForm.get('chantier_id').setValue(ar.chantier_id);
+		// this.arForm.get('a_prevoir_compagnons').setValue(ar.a_prevoir_compagnons+'');
+		// this.arForm.get('a_signer_registre_travaux').setValue(ar.a_signer_registre_travaux+'');
+		// this.arForm.get('a_prevoir_balisage').setValue(ar.a_prevoir_balisage+'');
 	}
 
 	isLastStep(isLastStep: boolean): void {
