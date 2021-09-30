2e696e842d337aefd6fff03a2800149c2205bd7c
ihssane hanzaz <i.hanzaz@piman-group.fr>
2021-09-30 16:03:02 +0100

    admin epi disposition 2

---
+++ src/app/views/partials/layout/admins/pdp-epi-disposition/pdp-epi-disposition.component.ts
@@ -0,0 +1,87 @@
+import {ChangeDetectorRef, Component, Injector, OnInit} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {TranslateService} from '@ngx-translate/core';
+import {AdminTemplateComponent} from '@app/views/partials/layout/admin-template/admin-template.component';
+import {EpiDispositionService} from '@app/core/services';
+
+@Component({
+	selector: 'tf-pdp-epi-disposition',
+	templateUrl: './pdp-epi-disposition.component.html',
+	styleUrls: ['./pdp-epi-disposition.component.scss']
+})
+export class PdpEpiDispositionComponent extends AdminTemplateComponent implements OnInit {
+
+	cdr: ChangeDetectorRef;
+	modalService: NgbModal;
+	parentService: any;
+	childService: any;
+	translate: TranslateService;
+
+	tpl: any;
+
+	list: any[];
+
+	constructor(injector: Injector) {
+		super(injector);
+		this.cdr = injector.get(ChangeDetectorRef);
+		this.modalService = injector.get(NgbModal);
+		this.parentService = injector.get(EpiDispositionService);
+		this.translate = injector.get(TranslateService);
+	}
+
+	ngOnInit() {
+		super.ngOnInit();
+		this.refreshTranslations();
+		this.tpl = {
+			title: this.translate.instant('PDP.CARD.EPI_DISPOSITION_EES.SHORTTITLE'),
+			deletedMessage: this.translate.instant('PDP.NOTIF.ELEMENT_NOT_DELETED.TITLE'),
+			deletedChildMessage: this.translate.instant('PDP.NOTIF.ELEMENT_NOT_DELETED.SUBTITLE'),
+			collapsed: true,
+			canUpdateTitle: false,
+			titleOject: null,
+			childCol: 12
+		};
+	}
+
+	refreshTranslations() {
+		this.translate.stream('PDP.CARD.EPI_DISPOSITION_EES.SHORTTITLE').subscribe(x => {
+			this.tpl.title = x;
+		});
+		this.translate.stream('PDP.NOTIF.ELEMENT_NOT_DELETED.TITLE').subscribe(x => {
+			this.tpl.deletedMessage = x;
+		});
+		this.translate.stream('PDP.NOTIF.ELEMENT_NOT_DELETED.SUBTITLE').subscribe(x => {
+			this.tpl.deletedChildMessage = x;
+		});
+	}
+
+	async getList() {
+		try {
+			const res = await this.parentService.getAllAsAdmin('epi_disposition_ees').toPromise();
+			this.list = res.result.data;
+			this.cdr.markForCheck();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async addItem() {
+		await super.addItem(this.translate.instant('PDP.ACTION.ADD_EPI_DISPOSITION'), {
+			ordre: this.generateParentOrdre(),
+			active: 1
+		}, false);
+	}
+
+	async deleteItem({id}) {
+		super.deleteItem({id}, {title: this.translate.instant('PDP.NOTIF.EPI_DISPOSITION_ARCHIVED.TITLE')});
+	}
+
+	async updateOrders(datas) {
+		try {
+			await this.parentService.updateOrders({data: datas, type: 'epi_disposition_ees'}).toPromise();
+			this.cdr.markForCheck();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
