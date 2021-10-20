edc82c18bf63c73a85bc3ec0da3feda693d50b96
Owein Dourneau <o.dourneau@piman-group.fr>
2021-10-20 17:30:04 +0200

    Fix Pdp change type

--- src/app/views/pages/plan-prevention/pdp-add/pdp-add.component.ts
+++ src/app/views/pages/plan-prevention/pdp-add/pdp-add.component.ts
@@ -332,7 +332,7 @@ export class PdpAddComponent implements OnInit, OnDestroy {
 				read_and_approved.updateValueAndValidity({emitEvent:false})
 			})
 	 }
-	 
+
 	}
 
 
@@ -462,10 +462,13 @@ export class PdpAddComponent implements OnInit, OnDestroy {
 	}
 
 	selectPdp(typePdp : string){
+		if(typePdp != this.typePdp){
+			this.createForm();
+			// this.createClientForm();
+		}
 		this.typePdp = typePdp;
 		this.modalService.dismissAll();
-		this.createForm();
-		this.createClientForm();
+
 	}
 
 	toFormData<T>( formValue: T ) {
